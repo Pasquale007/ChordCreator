6948f896a279dc3a13d7187cdad441041bdef9c8
Pasquale007 <73184231+Pasquale007@users.noreply.github.com>
2024-03-07 19:10:58 +0100

    remove unused imports

--- Config/Config_Example.cs
+++ Config/Config_Example.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChordCreater.Config {
+﻿namespace ChordCreater.Config {
     internal class Config_Example {
         public static readonly string targetFolder = "path-to-target-folder";
     }
